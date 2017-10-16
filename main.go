7896a8d609af00471e983515d760819ba0d14e5a
justronaut <justronaut@gmail.com>
2017-10-17 02:54:32 +0900

    init

---
+++ main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/paypal/gatt"
+	"github.com/paypal/gatt/examples/option"
+)
+
+var done = make(chan struct{})
+
+func onStateChanged(d gatt.Device, s gatt.State) {
+	fmt.Println("State:", s)
+	switch s {
+	case gatt.StatePoweredOn:
+		fmt.Println("Scanning...")
+		d.Scan([]gatt.UUID{}, false)
+		return
+	default:
+		d.StopScanning()
+	}
+}
+
+func onPeriphDiscovered(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
+	id := strings.ToUpper(flag.Args()[0])
+	if strings.ToUpper(p.ID()) != id {
+		return
+	}
+
+	// Stop scanning once we've got the peripheral we're looking for.
+	p.Device().StopScanning()
+
+	p.Device().Connect(p)
+}
+
+const SwitchBotServiceUUID = "cba20d00224d11e69fb80002a5d5c51b"
+const SwitchBotCharacteristicsUUID = "cba20002224d11e69fb80002a5d5c51b"
+
+func onPeriphConnected(p gatt.Peripheral, err error) {
+	defer p.Device().CancelConnection(p)
+
+	if err := p.SetMTU(500); err != nil {
+		fmt.Printf("Failed to set MTU, err: %s\n", err)
+	}
+
+	// Discovery services
+	ss, err := p.DiscoverServices(nil)
+	if err != nil {
+		fmt.Printf("Failed to discover services, err: %s\n", err)
+		return
+	}
+
+	for _, s := range ss {
+		if s.UUID().String() != SwitchBotServiceUUID {
+			continue
+		}
+		msg := "Service: " + s.UUID().String()
+		if len(s.Name()) > 0 {
+			msg += " (" + s.Name() + ")"
+		}
+		fmt.Println(msg)
+
+		// Discovery characteristics
+		cs, err := p.DiscoverCharacteristics(nil, s)
+		if err != nil {
+			fmt.Printf("Failed to discover characteristics, err: %s\n", err)
+			continue
+		}
+
+		for _, c := range cs {
+			if c.UUID().String() != SwitchBotCharacteristicsUUID {
+				continue
+			}
+			msg := "  Characteristic  " + c.UUID().String()
+			if len(c.Name()) > 0 {
+				msg += " (" + c.Name() + ")"
+			}
+			msg += "\n    properties    " + c.Properties().String()
+			fmt.Println(msg)
+
+			// Write the characteristic to make run
+			if (c.Properties() & gatt.CharWrite) != 0 {
+				msg, _ := hex.DecodeString("570100") // 570100 is known property
+				err := p.WriteCharacteristic(c, msg, false)
+				if err != nil {
+					fmt.Printf("Failed to write characteristic, err: %s\n", err)
+					continue
+				}
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func onPeriphDisconnected(p gatt.Peripheral, err error) {
+	close(done)
+}
+
+func main() {
+	flag.Parse()
+	if len(flag.Args()) != 1 {
+		log.Fatalf("usage: %s [options] peripheral-id\n", os.Args[0])
+	}
+
+	d, err := gatt.NewDevice(option.DefaultClientOptions...)
+	if err != nil {
+		log.Fatalf("Failed to open device, err: %s\n", err)
+		return
+	}
+
+	// Register handlers.
+	d.Handle(
+		gatt.PeripheralDiscovered(onPeriphDiscovered),
+		gatt.PeripheralConnected(onPeriphConnected),
+		gatt.PeripheralDisconnected(onPeriphDisconnected),
+	)
+
+	d.Init(onStateChanged)
+	<-done
+	fmt.Println("Done")
+}
